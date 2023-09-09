24940c499f72d6f59e4db3ec9ae40a46937bc3db
Eric Wishnu S <eric.wishnu@gmail.com>
2023-09-09 16:26:05 +0700

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
     <title>Lomba Mewarnai BETADINE</title>
     {{-- @vite('resources/css/app.css') --}}
 
