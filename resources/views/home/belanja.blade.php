2abd97e210d9489669d3d3016d00efa0e4a6e8de
Eric Wishnu S <eric.wishnu@gmail.com>
2023-09-13 00:44:10 +0700

    update footer and belanja section

--- resources/views/home/belanja.blade.php
+++ resources/views/home/belanja.blade.php
@@ -4,102 +4,105 @@
             <div class="mx-auto w-full">
                 <img src="/images/belanja/belanja-title.png" class="mx-auto h-20">
 
-                <div class="relative mt-14 grid grid-cols-1 xs:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 z-10">
+                <div class="relative max-w-5xl mx-auto mt-14 grid grid-cols-1 xs:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 z-10">
                     <div class="bg-white rounded-3xl m-4">
-                        <h2 class="p-10 text-xl font-bold text-center">BELANJA PRODUK BETADINE WOUND CARE</h2>
-                        <div class="grid grid-cols-1 lg:grid-cols-6">
-                            <div class="p-6 lg:col-span-2 ">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/solution15ml.png"
-                                        class="h-32 p-2 mx-auto object-contain object-center">
+                        <div class="overflow-x-auto flex">
+                            <div class="flex-none w-full">
+                                <h2 class="p-10 text-xl font-bold text-center">BELANJA PRODUK BETADINE WOUND CARE</h2>
+                                <div class="grid grid-cols-1 lg:grid-cols-6">
+                                    <div class="p-6 lg:col-span-2 ">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/solution15ml.png"
+                                                class="h-32 p-2 mx-auto object-contain object-center">
+
+                                        </div>
+                                        <div class="text-center">Betadine Antiseptic Solution</div>
+                                    </div>
+                                    <div class="p-6 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/ointment10ml.png"
+                                                class="h-32  mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Antiseptic Ointment</div>
+                                    </div>
+                                    <div class="p-6 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/elastic-fabric.png"
+                                                class="h-32 p-2 mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Plaster Elastic Fabric</div>
+                                    </div>
+
+                                    <div class="p-6 lg:col-start-2 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/waterproof-transparent.png"
+                                                class="h-32 p-2 mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Plaster Waterproof Transparent</div>
+                                    </div>
+
+                                    <div class="p-6 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/skin-cleanser.png"
+                                                class="h-32 p-2 mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Antiseptic Skin Cleanser</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div  class="flex-none w-full">
+                                <h2 class="p-10 text-xl font-bold text-center">PRODUK BETADINE LAINNYA</h2>
+                                <div class="grid grid-cols-1 lg:grid-cols-6">
+                                    <div class="p-6 lg:col-span-2 ">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/lozenges-depan.png"
+                                                class="h-32 p-2 mx-auto object-contain object-center">
+
+                                        </div>
+                                        <div class="text-center">Betadine Lozenges Menthol & Ecalyptus Flavor</div>
+                                    </div>
+                                    <div class="p-6 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/loz-orange-depan.png"
+                                                class="h-32  mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Relieving Orange Flavor</div>
+                                    </div>
+                                    <div class="p-6 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/betadine-mouthwash-and-gargle.png"
+                                                class="h-32 p-2 mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Mouthwash and Gargle</div>
+                                    </div>
+
+                                    <div class="p-6 lg:col-start-2 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/betadine-nasal-spray.png"
+                                                class="h-32 p-2 mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Nasal Spray</div>
+                                    </div>
+
+                                    <div class="p-6 lg:col-span-2">
+                                        <div class="bg-gray-200 mx-4 p-6">
+                                            <img src="/images/belanja/betadine-throat-spray-tampak-depan-samping-batch.png"
+                                                class="h-32 p-2 mx-auto object-contain">
+                                        </div>
+                                        <div class="text-center">Betadine Throat Spray</div>
+                                    </div>
 
                                 </div>
-                                <div class="text-center">Betadine Antiseptic Solution</div>
-                            </div>
-                            <div class="p-6 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/ointment10ml.png" class="h-32  mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Antiseptic Ointment</div>
-                            </div>
-                            <div class="p-6 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/elastic-fabric.png"
-                                        class="h-32 p-2 mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Plaster Elastic Fabric</div>
-                            </div>
-
-                            <div class="p-6 lg:col-start-2 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/waterproof-transparent.png"
-                                        class="h-32 p-2 mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Plaster Waterproof Transparent</div>
-                            </div>
-
-                            <div class="p-6 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/skin-cleanser.png"
-                                        class="h-32 p-2 mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Antiseptic Skin Cleanser</div>
                             </div>
 
                         </div>
+
                         <a href="https://shopee.co.id/betadineofficial?smtt=9&utm_campaign=s63813269_ss_id_webs_betadinecolouringcompetition&utm_content=betadinecolouringcompetition&utm_medium=seller&utm_source=website#product_list"
                             target="_blank">
                             <img src="/images/belanja/button-beli.png" class="h-10 mx-auto object-contain mb-14">
                         </a>
                     </div>
 
-                    <div class="bg-white rounded-3xl m-4">
-                        <h2 class="p-10 text-xl font-bold text-center">PRODUK BETADINE LAINNYA</h2>
-                        <div class="grid grid-cols-1 lg:grid-cols-6">
-                            <div class="p-6 lg:col-span-2 ">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/lozenges-depan.png"
-                                        class="h-32 p-2 mx-auto object-contain object-center">
-
-                                </div>
-                                <div class="text-center">Betadine Lozenges Menthol & Ecalyptus Flavor</div>
-                            </div>
-                            <div class="p-6 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/loz-orange-depan.png" class="h-32  mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Relieving Orange Flavor</div>
-                            </div>
-                            <div class="p-6 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/betadine-mouthwash-and-gargle.png"
-                                        class="h-32 p-2 mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Mouthwash and Gargle</div>
-                            </div>
-
-                            <div class="p-6 lg:col-start-2 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/betadine-nasal-spray.png"
-                                        class="h-32 p-2 mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Nasal Spray</div>
-                            </div>
-
-                            <div class="p-6 lg:col-span-2">
-                                <div class="bg-gray-200 mx-4 p-6">
-                                    <img src="/images/belanja/betadine-throat-spray-tampak-depan-samping-batch.png"
-                                        class="h-32 p-2 mx-auto object-contain">
-                                </div>
-                                <div class="text-center">Betadine Throat Spray</div>
-                            </div>
-
-                        </div>
-                        <a href="https://shopee.co.id/betadineofficial?smtt=9&utm_campaign=s63813269_ss_id_webs_betadinecolouringcompetition&utm_content=betadinecolouringcompetition&utm_medium=seller&utm_source=website#product_list"
-                            target="_blank">
-                            <img src="/images/belanja/button-beli.png" class="h-10 mx-auto object-contain mb-14">
-                        </a>
-                    </div>
                     <!-- More posts... -->
                 </div>
             </div>
--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -3,10 +3,10 @@
     <div class="bg-[#ece8e4] ">
 
         <div class="mx-auto max-w-7xl  py-8 sm:py-8 ">
-            <div class="flex mx-auto justify-center">
-                <img src="/images/betadine-black.png" class=" h-6">
+            <div class="flex flex-col md:flex-row mx-auto justify-center">
+                <img src="/images/betadine-black.png" class=" h-6 w-auto sm:mx-8 md:mx-0 mx-8 object-contain">
 
-                <span>is a Registered Trademark of Mundipharma | ID-875/BETA/299/1118 </span>
+                <span class="sm:mx-8 md:mx-0 mx-8 sm:text-center text-center md:text-left">is a Registered Trademark of Mundipharma | ID-875/BETA/299/1118 </span>
             </div>
         </div>
     </div>
